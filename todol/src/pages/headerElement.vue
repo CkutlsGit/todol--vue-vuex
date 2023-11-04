56ac3d9f3e3a11511a733e37d8c6f1dd350c794e
Kostya <ckutls228@gmail.com>
2023-11-04 18:40:26 +0300

    feat: add header file

---
+++ todol/src/pages/headerElement.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <h1>ToDo List</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'headerElement'
+}
+</script>
+
+<style scoped>
+
+</style>
