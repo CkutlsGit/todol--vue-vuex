65517993319244431a8a4da6e98f835d70fec76e
Kostya <ckutls228@gmail.com>
2023-11-04 17:25:33 +0300

    feat: add vision for todo in home page

--- todol/src/pages/HomePage.vue
+++ todol/src/pages/HomePage.vue
@@ -3,8 +3,22 @@
     <h1>Home Page</h1>
     <a href="#" @click="$router.push({ name: 'todol' })">go todol</a>
   </div>
+  <aside class="todo-list">
+    <h2>Ваши последние задания</h2>
+    <ul>
+      <li
+          v-for="mission in $store.state.List"
+          :key="mission.id"
+      >
+        <h4>{{ mission.title }}</h4>
+        <h4>{{ mission.description }}</h4>
+      </li>
+    </ul>
+  </aside>
 </template>
 
 <style scoped>
 
 </style>
+<script setup>
+</script>
