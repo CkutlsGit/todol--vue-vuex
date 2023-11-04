964d88c8583f8e09756518b079aa50b1f0969d45
Kostya <ckutls228@gmail.com>
2023-11-04 16:33:09 +0300

    feat: add modal window file

---
+++ todol/src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="modal">
+    <input type="text">
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  data() {
+    return {
+      title: '',
+      description: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
