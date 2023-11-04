e3ec0e86ffb670a3d09ba40678980d8df2e323c6
Kostya <ckutls228@gmail.com>
2023-11-04 16:09:14 +0300

    refactor: add import and change name for component in home path

--- todol/src/router/router.js
+++ todol/src/router/router.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import HomePage from '../pages/HomePage.vue'
+import TodoPage from "@/components/TodoPage.vue";
+
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: homePage, name: 'home'},
-        { path: '/todo', component: ToDoPage, name: 'todol' }
+        { path: '/', component: HomePage, name: 'home'},
+        { path: '/todo', component: TodoPage, name: 'todol' }
     ]
 })
