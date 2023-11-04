10c5005ea8034021b3283c3a1236f4011be703f6
Kostya <ckutls228@gmail.com>
2023-11-04 15:55:25 +0300

    feat: add router file and him settings

---
+++ todol/src/router/router.js
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: homePage, name: home },
+        { path: '/todo', component: ToDoPage, name: todol }
+    ]
+})
