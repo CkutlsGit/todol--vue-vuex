35817d9af276cabc4c490b15cde26c714fe4c28a
Kostya <ckutls228@gmail.com>
2023-11-04 16:57:21 +0300

    feat: add import store

--- todol/src/main.js
+++ todol/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import router from "@/router/router";
+import store from "@/store/index"
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
 
