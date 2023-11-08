6a422f4969f4aba9a622ce003cfaed18cd018786
Kostya <ckutls228@gmail.com>
2023-11-08 20:18:53 +0300

    style: change template

--- todol/src/pages/HomePage.vue
+++ todol/src/pages/HomePage.vue
@@ -5,19 +5,21 @@
       <h1>Home Page</h1>
       <a href="#" @click="$router.push({ name: 'todol' })">go todol</a>
     </div>
+    <div class="missions__content">
+      <aside class="todo-list" @click="$router.push({ name: 'todol' })">
+        <h2>Ваши последние задания</h2>
+        <ul>
+          <li
+              v-for="mission in getList"
+              :key="mission.id"
+          >
+            <h4>{{ mission.title }}</h4>
+            <h4>{{ mission.description }}</h4>
+          </li>
+        </ul>
+      </aside>
+    </div>
   </div>
-  <aside class="todo-list" @click="$router.push({ name: 'todol' })">
-    <h2>Ваши последние задания</h2>
-    <ul>
-      <li
-          v-for="mission in getList"
-          :key="mission.id"
-      >
-        <h4>{{ mission.title }}</h4>
-        <h4>{{ mission.description }}</h4>
-      </li>
-    </ul>
-  </aside>
 </template>
 
 <script>
